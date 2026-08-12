1a8ae4ed16e23afc77a6ad56df9bea6648abdc94
Marek Marcola <Marek.Marcola@gmail.com>
2026-08-12 18:30:25 +0200

    cdev.sh: add support for OCI registers to DELETE-REG stage
    Change-Id: I532dd8c771ad226591ea42ca93e2d71a4a3ae30a

--- cdev.sh
+++ cdev.sh
@@ -878,18 +878,22 @@ if [ $DELR -ne 0 ]; then
     exit 1
   fi
 
+  H="-H Accept:application/vnd.oci.image.manifest.v1+json"
+  H="$H -H Accept:application/vnd.oci.image.index.v1+json"
+  H="$H -H Accept:application/vnd.docker.distribution.manifest.v2+json"
+
   for r in $REGISTRY_HOST; do
     [[ "$REGP" != "" ]] && FREPO=$REGP/$REPO || FREPO=$REPO
     echo "[$r/$FREPO]"
     TAGS=$(curl --netrc-file $REGISTRY_AUTH -s -k -L $r/v2/$FREPO/tags/list|jq|grep -E '\.[0-9]{10}'|xargs -L1|
       sed 's/,$//' |awk -F. '{print $NF,$0}'|sed 's/^20//'|sort -nr|cut -f2- -d' '|sed "1,${DELR}d"|sort -n)
     for TAG in $TAGS; do
-      DCD=$(curl --netrc-file $REGISTRY_AUTH -s -I -k -H "Accept:application/vnd.docker.distribution.manifest.v2+json" $r/v2/$FREPO/manifests/$TAG|
+      DCD=$(curl --netrc-file $REGISTRY_AUTH -s -I -k $H $r/v2/$FREPO/manifests/$TAG|
         grep -i docker-content-digest|awk '{print $2}' | tr -d "\t\r\n")
       if [ "$DCD" != "" ]; then
         echo "# $TAG"
         if [ $EVAL -ne 0 ]; then
-          echo | xargs -L1 -t curl --netrc-file $REGISTRY_AUTH -k -H "Accept:application/vnd.docker.distribution.manifest.v2+json" -X DELETE $r/v2/$FREPO/manifests/$DCD
+          echo | xargs -L1 -t curl --netrc-file $REGISTRY_AUTH -k $H -X DELETE $r/v2/$FREPO/manifests/$DCD
         fi
       fi
     done
